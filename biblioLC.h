7f7a798b8a069e6f2d2c94a0f5ffbdbbe69d5837
Danaël <98533285+CyranHOEdeBergerac@users.noreply.github.com>
2022-02-08 15:54:25 +0100

    c plus bo

--- biblioLC.h
+++ biblioLC.h
@@ -12,10 +12,19 @@ typedef struct { /* Tete fictive */
 	Livre * L ; /* Premier element */
 } Biblio ;
 
+/*Fonction permettant de créer un livre à l'aide de son numéro, d'un titre, et d'un auteur et retourne un pointeur sur la structure*/
 Livre* creer_livre(int num, char* titre, char* auteur);
+
+/*Fonction permettant de libérer la place allouée en mémoire au Livre l*/
 void liberer_livre(Livre* l);
+
+/*Créé une bibliothèque en initialisant son champ livre à null*/
 Biblio* creer_biblio();
+
+/*Libère la Biblio b allouée*/
 void liberer_biblio(Biblio* b);
+
+/*Fonction permettant d'insérer un livre caractérisé par son numéro, son titre et son auteur en tête d'une Biblio b*/
 void inserer_en_tete(Biblio* b, int num, char* titre, char* auteur);
 
 
