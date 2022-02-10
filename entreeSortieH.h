f0025fd3bac08dfe44ad7fb2d1c833c630c93c2a
Danaël <98533285+CyranHOEdeBergerac@users.noreply.github.com>
2022-02-10 23:45:21 +0100

    Un peu de mise en page

--- entreeSortieH.h
+++ entreeSortieH.h
@@ -1,7 +1,10 @@
 #ifndef ENTREESORTIEH_H
 #define ENTREESORTIEH_H
 
+/*Fonction permettant de charger n entrées à partir d'un fichier nommé nomfic*/
 BiblioH* charger_n_entrees(char* nomfic, int n, int m);
+
+/*Fonction permettant de passer d'une bibliothèque à un fichier nummé nomfic (enregistrer la bibliothèque)*/
 void enregistrer_biblio(BiblioH *bH, char* nomfic);
 
 #endif
